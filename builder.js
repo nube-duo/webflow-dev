e9009c95b5b58dad7e464304f5d9578d5c0726f0
stefan-design <savanovic.design@gmail.com>
2022-12-09 15:51:08 +0100

    first commit

---
+++ builder.js
@@ -0,0 +1,10 @@
+require("esbuild")
+  .build({
+    entryPoints: ["index.js"],
+    bundle: true,
+    minify: true,
+    watch: true,
+    sourcemap: true,
+    outfile: "dist/index.js",
+  })
+  .catch(() => process.exit(1));
